563c9bdc94c42a6f4570b4ab8e0b2e78b299544e
David Culemann <davidculemann@gmail.com>
2024-12-08 15:16:56 +0000

    fix darktrace cloud

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -10,6 +10,7 @@ type Props = {
         title: string;
         description: string;
         logo?: string;
+        repository?: string;
     };
 
     views: number;
@@ -17,6 +18,20 @@ type Props = {
 export const Header: React.FC<Props> = ({ project, views }) => {
     const ref = useRef<HTMLElement>(null);
     const [isIntersecting, setIntersecting] = useState(true);
+
+    const links: { label: string; href: string }[] = [];
+    if (project.repository) {
+        links.push({
+            label: "GitHub",
+            href: `https://github.com/${project.repository}`,
+        });
+    }
+    if (project.url) {
+        links.push({
+            label: "Website",
+            href: project.url,
+        });
+    }
 
     useEffect(() => {
         if (!ref.current) return;
@@ -70,6 +85,16 @@ export const Header: React.FC<Props> = ({ project, views }) => {
                             )}
                         </span>
                         <p className="mt-6 text-lg leading-8 text-muted-foreground">{project.description}</p>
+                    </div>
+
+                    <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+                        <div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+                            {links.map((link) => (
+                                <Link target="_blank" key={link.label} href={link.href}>
+                                    {link.label} <span aria-hidden="true">&rarr;</span>
+                                </Link>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
