1e259a54b50f011c09cc0b7c481a64e23b813907
David Culemann <davidculemann@gmail.com>
2024-12-08 17:14:50 +0000

    css fixes

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,14 +1,30 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import { Navigation } from "../components/nav";
 import { Download } from "lucide-react";
+import { useEffect, useState } from "react";
+import { cn } from "../lib/utils";
 
 export default function CVPage() {
+    const { theme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
     return (
         <div className="relative min-h-screen bg-gradient-to-tl from-background/0 via-background to-background/0">
             <Navigation />
-            <div className="container flex flex-col justify-center mx-auto h-screen w-full pt-24 px-4 lg:px-48">
-                <iframe src="/cv.pdf#view=FitH&toolbar=0&navpanes=0&scrollbar=0" className="w-full h-full rounded-md" />
+            <div className="container flex flex-col justify-center mx-auto h-screen w-full pt-24 px-4 lg:px-48 items-center">
+                <iframe
+                    src="/cv.pdf#view=FitH&toolbar=0&navpanes=0&scrollbar=0"
+                    className={cn(
+                        "w-full h-full rounded-md max-w-screen-md",
+                        mounted && theme === "dark" ? "[filter:invert(1)_hue-rotate(180deg)]" : ""
+                    )}
+                />
                 <a
                     href="/cv.pdf"
                     download
