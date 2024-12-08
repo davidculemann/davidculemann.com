dcbbc0c45a6725172598f1587eb43747b8e9cf10
David Culemann <davidculemann@gmail.com>
2024-12-08 17:22:52 +0000

    add download button

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -2,9 +2,10 @@
 
 import { useTheme } from "next-themes";
 import { Navigation } from "../components/nav";
-import { Download } from "lucide-react";
+import { Download, FileIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { cn } from "../lib/utils";
+import { Button } from "../components/ui/button";
 
 export default function CVPage() {
     const { theme } = useTheme();
@@ -17,22 +18,21 @@ export default function CVPage() {
     return (
         <div className="relative min-h-screen bg-gradient-to-tl from-background/0 via-background to-background/0">
             <Navigation />
-            <div className="container flex flex-col justify-center mx-auto h-screen w-full pt-24 px-4 lg:px-48 items-center">
+            <div className="container flex flex-col gap-4 justify-center mx-auto h-screen w-full pt-24 pb-12 px-4 lg:px-48 items-center">
                 <iframe
-                    src="/cv.pdf#view=FitH&toolbar=0&navpanes=0&scrollbar=0"
+                    src="/cv-david-culemann.pdf#view=FitH&toolbar=0&navpanes=0&scrollbar=0"
                     className={cn(
                         "w-full h-full rounded-md max-w-screen-md",
                         mounted && theme === "dark" ? "[filter:invert(1)_hue-rotate(180deg)]" : ""
                     )}
                 />
-                <a
-                    href="/cv.pdf"
-                    download
-                    className="flex items-center gap-2 px-4 py-2 text-sm text-foreground/80 hover:text-foreground duration-500"
-                >
-                    <Download size={16} />
-                    Download CV
-                </a>
+
+                <Button asChild>
+                    <a href="/cv-david-culemann.pdf" download className="flex items-center gap-2 px-4 py-2">
+                        <Download size={16} />
+                        Download CV
+                    </a>
+                </Button>
             </div>
         </div>
     );
